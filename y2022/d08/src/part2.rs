18820b4c92a3647255213dd4ce2eb82e872926d0
CircuitSacul <circuitsacul@gmail.com>
2022-12-08 20:40:29 -0500

    y2022d08: p2 improvements

--- y2022/d08/src/part2.rs
+++ y2022/d08/src/part2.rs
@@ -13,53 +13,51 @@ pub fn main() {
             .map(move |(x, c)| (x, y, c as usize - 48))
     });
 
-    let mut tree_values = Vec::new();
+    let mut lines = content.lines();
+    let max_x = lines.next().unwrap().len() - 1;
+    let max_y = lines.count();  // + 1 - 1 == 0
+
+    let mut tree_values = Vec::with_capacity((max_x + 1) * (max_y + 1));
 
     let mut x_by_height: [usize; 10] = [0; 10];
-    let mut y_by_x_height: Vec<[usize; 10]> = Vec::new();
-    let mut last_y = 0;
-    let mut max_x = 0;
+    let mut y_by_x_height: Vec<[usize; 10]> = Vec::with_capacity(max_x + 1);
+    for _ in 0..=max_x {
+        y_by_x_height.push([0; 10]);
+    }
     for (x, y, h) in forest_iter {
-        if x > max_x {
-            max_x = x;
-        }
-
-        if x >= y_by_x_height.len() {
-            y_by_x_height.push([0; 10]);
-        }
-
-        if last_y != y {
-            last_y = y;
+        if x == 0 {
             x_by_height = [0; 10];
         }
 
+        let y_by_height = &mut y_by_x_height[x];
+
         let x_back = x - x_by_height[h];
-        let y_back = y - y_by_x_height[x][h];
+        let y_back = y - y_by_height[h];
 
         for h in 0..=h {
             x_by_height[h] = x;
-            y_by_x_height[x][h] = y;
+            y_by_height[h] = y;
         }
 
         tree_values.push((x, y, h, y_back * x_back));
     }
-    println!();
 
-    let mut x_by_height: [usize; 10] = [max_x; 10];
+    x_by_height = [max_x; 10];
     y_by_x_height.iter_mut().for_each(|v| *v = [max_x; 10]);
     let mut best = 0;
     for (x, y, h, ul_val) in tree_values.into_iter().rev() {
-        if last_y != y {
-            last_y = y;
-            x_by_height = [x; 10];
+        if x == max_x {
+            x_by_height = [max_x; 10];
         }
 
+        let y_by_height = &mut y_by_x_height[x];
+
         let x_back = x_by_height[h] - x;
-        let y_back = y_by_x_height[x][h] - y;
+        let y_back = y_by_height[h] - y;
 
         for h in 0..=h {
             x_by_height[h] = x;
-            y_by_x_height[x][h] = y;
+            y_by_height[h] = y;
         }
 
         let this = ul_val * y_back * x_back;
